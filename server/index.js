5f6de5d57276eb9629ce8acc80b0a05eac63d96f
Noal Zyglowicz <ntz3sw@virginia.edu>
2021-12-01 23:55:22 -0500

    chagne to res.send instead of res.json

--- server/index.js
+++ server/index.js
@@ -12,7 +12,8 @@ app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+  //res.json("Hello from server!");
+  res.send({ title: "GeeksforGeeks" });
 });
 
 // All other GET requests not handled before will return our React app
